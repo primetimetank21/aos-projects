5f48c913242d491592f1aacc48a6b7b0cbf92ffd
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-03-23 02:53:20 -0400

    docs: improved documentation

--- project3/proj3.py
+++ project3/proj3.py
@@ -6,7 +6,11 @@ import math
 import os
 from random import randint
 
+
 def print_current_values(name, arr, idx_changed) -> None:
+    """
+    Print values in the given array (and highlight the value that has been changed)
+    """
     print("Shared array after " + colored(f"Process {name}", "blue") + "'s operation:",end="\t[")
     for i in range(len(arr)):
         if i == idx_changed:
@@ -15,14 +19,21 @@ def print_current_values(name, arr, idx_changed) -> None:
             print(colored(f"{f'{str(arr[i]):<5}' if len(str(arr[i])) <= 5 else f'{str(arr[i])[:5]}...'}", "cyan"), end=" ")
     print("]\t(modified " +  colored(f"arr[","red") + colored(str(idx_changed), "green") + colored("]", "red") + ")\n")
 
+
 def print_current_values_parent(pos, arr) -> None:
-    print(f"Shared array at {pos}:\t[",end="")
-    # for element in arr:
+    """
+    Print values in the given array
+    """
+    print(f"Shared array at {pos}:\t[",end=" ")
     for i in range(len(arr)):
         print(colored(f"{str(arr[i])}", "cyan"), end=" ")
     print("]\n")
 
+
 def add(blk_name, semaphore, shape, iterations) -> None:
+    """
+    Process that increments values in the Shared array
+    """
     #access intial shared copy
     existing_shm = shared_memory.SharedMemory(name=blk_name, create=False)
     arr = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
@@ -30,16 +41,23 @@ def add(blk_name, semaphore, shape, iterations) -> None:
     for _ in range(iterations):
         num = randint(1, max(arr))
         idx = randint(0,arr.size-1)
+
+        #critical section
         with semaphore:
             arr[idx] += num
             print(colored(f"Process {pid}", "blue") + " added " + colored(str(num), "yellow") + " to " + colored(str(arr[idx] - num), "red") + " (result: " + colored(str(arr[idx]), "red") + ")")
             print_current_values(pid, arr, idx)
         sleep(randint(1,3))
+    
     #clean up
     existing_shm.close()
     exit(0)
 
+
 def sub(blk_name, semaphore, shape, iterations) -> None:
+    """
+    Process that decrements values in the Shared array
+    """
     #access intial shared copy
     existing_shm = shared_memory.SharedMemory(name=blk_name, create=False)
     arr = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
@@ -47,41 +65,55 @@ def sub(blk_name, semaphore, shape, iterations) -> None:
     for _ in range(iterations):
         num = randint(1, max(arr))
         idx = randint(0,arr.size-1)
+
+        #critical section
         with semaphore:
             arr[idx] -= num
             print(colored(f"Process {pid}", "blue") + " subtracted " +  colored(str(num), "yellow") + " from " + colored(str(arr[idx] + num), "red") + " (result: " + colored(str(arr[idx]), "red") + ")")
             print_current_values(pid, arr, idx)
         sleep(randint(1,3))
+
     #clean up
     existing_shm.close()
     exit(0)
 
 
 def square(blk_name, semaphore, shape, iterations) -> None:
+    """
+    Process that squares values in the Shared array
+    """
     #access intial shared copy
     existing_shm = shared_memory.SharedMemory(name=blk_name, create=False)
     arr = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
     pid = os.getpid()
     for _ in range(iterations):
         idx = randint(0,arr.size-1)
+
+        #critical section
         with semaphore:
             num = arr[idx]
             arr[idx] = num**2
             print(colored(f"Process {pid}", "blue") + " squared " + colored(str(num), "red") + " (result: " + colored(str(arr[idx]), "red") + ")")
             print_current_values(pid, arr, idx)
         sleep(randint(1,3))
+
     #clean up
     existing_shm.close()
     exit(0)
 
 
 def sqrt(blk_name, semaphore, shape, iterations) -> None:
+    """
+    Process that takes the square root of values in the Shared array
+    """
     #access intial shared copy
     existing_shm = shared_memory.SharedMemory(name=blk_name, create=False)
     arr = np.ndarray(shape, dtype=np.int64, buffer=existing_shm.buf)
     pid = os.getpid()
     for _ in range(iterations):
         idx = randint(0,arr.size-1)
+
+        #critical section
         with semaphore:
             num = arr[idx]
             if num < 0:
@@ -92,6 +124,7 @@ def sqrt(blk_name, semaphore, shape, iterations) -> None:
             print(colored(f"Process {pid}", "blue") + " took sqrt of " + colored(str(num), "red") + " (result: " + colored(str(arr[idx]), "red") + ")")
             print_current_values(pid, arr, idx)
         sleep(randint(1,3))
+
     #clean up
     existing_shm.close()
     exit(0)
@@ -102,10 +135,10 @@ def create_processes(params) -> list:
     Create processes that will modify shared memory variables
     """
     return [
-        Process(target=add, args=params),
-        Process(target=sub, args=params),
-        Process(target=square, args=params),
-        Process(target=sqrt, args=params)
+        Process(target=add, args=params),   #increments values
+        Process(target=sub, args=params),   #decrements values
+        Process(target=square, args=params),#squares values
+        Process(target=sqrt, args=params)   #square roots values
     ]
 
 
